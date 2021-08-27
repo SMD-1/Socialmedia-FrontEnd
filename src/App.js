408f73ac9d2a54b0c9a9c62f02c04effcc9b28cc
Danish <skdanish1999@gmail.com>
2021-08-27 23:07:34 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
-// import Login from "./Components/login/Login";
-// import Register from "./Components/register/Register";
+import Login from "./Components/login/Login";
+import Register from "./Components/register/Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <Profile />
-      {/* <Login /> */}
-      {/* <Register /> */}
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/profile" exact>
+            <Profile />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/register" exact>
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
