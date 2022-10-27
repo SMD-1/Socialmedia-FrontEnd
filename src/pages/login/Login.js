7d028cb602b7e32077beb744596401aec38a2ab0
Danish <skdanish1999@gmail.com>
2022-10-27 18:25:39 +0530

    change login and register page design

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,9 @@ import { useContext, useRef } from "react";
 import { loginCall } from "../../apiCall";
 import { AuthContext } from "../../context/AuthContext";
 import { useHistory } from "react-router-dom";
-import "./login.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../register/register.css";
 
 const Login = () => {
   // here we can also use useState
@@ -17,49 +19,68 @@ const Login = () => {
       { email: email.current.value, password: password.current.value },
       dispatch
     );
-    console.log(password.current.value);
+    // console.log(password.current.value);
   };
 
   const onClick = () => {
     history.push("/register");
   };
+  const diffToast = () => {
+    toast.warn("🦄 Wow so easy!", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
 
   console.log("user after login", user);
   return (
-    <div className="login">
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className="loginLogo">MAKAUTiians</h3>
-          <span className="logiDesc">Connect with you Seniors / Juniors </span>
+    <>
+      <div className="login">
+        <div className="top">
+          <h1 className="loginLogo">Login</h1>
+          <p>Welcome Back</p>
         </div>
-        <div className="loginRight">
+        <div className="loginForm">
           <form className="loginBox" onSubmit={submitHandler}>
+            <label for="username">Email</label>
             <input
+              id="email"
               className="loginInput"
               type="email"
-              placeholder="Email"
+              placeholder="Email *"
               required
               ref={email}
             />
+            <label for="password">Password</label>
             <input
+              id="password"
               className="loginInput"
               type="password"
-              placeholder="Password"
+              placeholder="Password *"
               minLength="6"
               required
               ref={password}
             />
-            <button className="loginButton" disabled={isFetching}>
+            <button
+              className="loginButton"
+              disabled={isFetching}
+              onClick={diffToast}
+            >
               {isFetching ? "Loading..." : "Login"}
             </button>
-            <span className="forgotPass">Forgot Password ?</span>
+            {/* <span className="forgotPass">Forgot Password ?</span> */}
             <button className="createNewAcc" onClick={onClick}>
               Create a new Account
             </button>
           </form>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,15 +1,25 @@
-import "./register.css";
-import { useRef } from "react";
-import { useHistory } from "react-router-dom";
+import { useContext, useEffect, useRef } from "react";
 import axios from "axios";
 import { prefix } from "../../apiconfig";
 
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { loginCall } from "../../apiCall";
+import { AuthContext } from "../../context/AuthContext";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./register.css";
+
 const Register = () => {
+  const { dispatch } = useContext(AuthContext);
   const username = useRef();
   const email = useRef();
   const password = useRef();
   const passwordAgain = useRef();
   const history = useHistory();
+
+  useEffect(() => {});
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -22,9 +32,38 @@ const Register = () => {
         password: password.current.value,
       };
       try {
-        await axios.post(prefix + "auth/register", user);
-        history.push("/login");
+        const newUser = await axios.post(prefix + "auth/register", user);
+        toast("Sussssses", {
+          position: "top-right",
+          type: "success",
+          theme: "dark",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+        loginCall(
+          {
+            email: email.current.value,
+            password: password.current.value,
+          },
+          dispatch
+        );
+        // TODO: redirect to login page
+        // history.push("/login");
       } catch (err) {
+        // yahan ayega error aur hoga toastify
+        toast("Couldn't register! Please try again ", {
+          position: "top-right",
+          type: "error",
+          theme: "dark",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
         console.log(err);
       }
     }
@@ -34,51 +73,57 @@ const Register = () => {
   };
   return (
     <div className="login">
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className="loginLogo">MAKAUTiians</h3>
-          <span className="logiDesc">Connect with you Seniors / Juniors </span>
-        </div>
-        <div className="loginRight">
-          <form className="loginBox" onSubmit={submitHandler}>
-            <input
-              type="text"
-              placeholder="username"
-              className="loginInput"
-              ref={username}
-              required
-            />
-            <input
-              className="loginInput"
-              type="email"
-              placeholder="Email"
-              ref={email}
-              required
-            />
-            <input
-              className="loginInput"
-              type="password"
-              placeholder="Password"
-              ref={password}
-              minLength="6"
-              required
-            />
-            <input
-              className="loginInput"
-              type="password"
-              placeholder="Confirm Password"
-              ref={passwordAgain}
-              required
-            />
-            <button className="loginButton" type="submit">
-              Signup
-            </button>
-            <p className="or">OR</p>
-            <button className="createNewAcc" onClick={onClick}>
-              Already have an account?
-            </button>
-          </form>
-        </div>
+      <div className="top">
+        <h1 className="loginLogo">Register</h1>
+        <p>Create an account</p>
+      </div>
+      <div className="loginForm">
+        <form className="loginBox" onSubmit={submitHandler}>
+          <label for="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            placeholder="username"
+            className="loginInput"
+            ref={username}
+            // required
+          />
+          <label for="email">Email</label>
+          <input
+            id="email"
+            className="loginInput"
+            type="email"
+            placeholder="Email"
+            ref={email}
+            // required
+          />
+          <label for="password">Password</label>
+          <input
+            id="password"
+            className="loginInput"
+            type="password"
+            placeholder="Password"
+            ref={password}
+            minLength="6"
+            // required
+          />
+          <label for="confpass">Confirm Password</label>
+          <input
+            id="confpass"
+            className="loginInput"
+            type="password"
+            placeholder="Confirm Password"
+            ref={passwordAgain}
+            // required
+          />
+          <button className="loginButton" type="submit">
+            Signup
+          </button>
+          <p className="or">OR</p>
+          <button className="createNewAcc" onClick={onClick}>
+            Already have an account?
+          </button>
+        </form>
       </div>
     </div>
   );
